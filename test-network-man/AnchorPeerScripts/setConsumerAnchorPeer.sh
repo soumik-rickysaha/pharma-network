835807089e25dbe91350727583c8f1d616907cdc
Soumik Saha <soumik.rickysaha@gmail.com>
2023-07-22 19:40:11 +0530

    New way of setting Anchor peer
    This commit has :-
    
    1) Commented the old way of setting the anchor peer in create_join_channel.sh
    
    2) Create a folder Anchor Peer Scripts, which contains individual scripts to setup anchor peers

---
+++ test-network-man/AnchorPeerScripts/setConsumerAnchorPeer.sh
@@ -0,0 +1,20 @@
+export ORDERER_CA=${PWD}/../organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/cacerts/ca.example.com-cert.pem
+export CORE_PEER_LOCALMSPID="ConsumerMSP"
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../organizations/peerOrganizations/Consumer.example.com/peers/peer0.Consumer.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/../organizations/peerOrganizations/Consumer.example.com/users/Admin@Consumer.example.com/msp/
+export CORE_PEER_ADDRESS=peer0.Consumer.example.com:7051
+peer channel fetch config config_block.pb -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c pharmachannel --cafile $ORDERER_CA
+
+configtxlator proto_decode --input config_block.pb --type common.Block | jq .data.data[0].payload.data.config >"${CORE_PEER_LOCALMSPID}config.json"
+export HOST="peer0.Consumer.example.com:"
+export PORT=7051
+jq '.channel_group.groups.Application.groups.'${CORE_PEER_LOCALMSPID}'.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "'$HOST'","port": '$PORT'}]},"version": "0"}}' ${CORE_PEER_LOCALMSPID}config.json > ${CORE_PEER_LOCALMSPID}modified_config.json
+
+configtxlator proto_encode --input "${CORE_PEER_LOCALMSPID}config.json" --type common.Config >original_config.pb
+configtxlator proto_encode --input "${CORE_PEER_LOCALMSPID}modified_config.json" --type common.Config >modified_config.pb
+configtxlator compute_update --channel_id "pharmachannel" --original original_config.pb --updated modified_config.pb >config_update.pb
+configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate >config_update.json
+echo '{"payload":{"header":{"channel_header":{"channel_id":"pharmachannel", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . >config_update_in_envelope.json
+configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope >"${CORE_PEER_LOCALMSPID}anchors.tx"
+
+peer channel update -o orderer.example.com:7050 --ordererTLSHostnameOverride orderer.example.com -c pharmachannel -f ${CORE_PEER_LOCALMSPID}anchors.tx  --cafile $ORDERER_CA
